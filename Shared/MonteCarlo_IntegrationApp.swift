497ff9d8a8749e1d4eb5853177db6994feff9a4b
Alaina Thompson <athompson9@hawk.iit.edu>
2022-02-04 11:26:12 -0600

    Initial Commit

---
+++ Shared/MonteCarlo_IntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonteCarlo_IntegrationApp.swift
+//  Shared
+//
+//  Created by Alaina Thompson on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MonteCarlo_IntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
